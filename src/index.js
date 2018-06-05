35826cb7d86f46aeb823e3fc57731a4fcfafcac0
LaToya Williams <latoyazamill@yahoo.com>
2018-06-05 08:06:14 -0700

    Complete main server file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 const app = require("express")();
 const bodyParser = require("body-parser");
+const routes = require("./server/routes");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extened: false}));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.use("/", routes);
+
 app.listen(3000, () => {
-  console.log("Server listening on port 3000...");
+  console.log(`Server listening on port http://localhost:3000...`);
 });

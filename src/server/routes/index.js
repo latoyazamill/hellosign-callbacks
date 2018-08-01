507110919dec182e82a05d09ec9c0cf7d1a0c0f5
LaToya Williams <latoyazamill@yahoo.com>
2018-08-01 10:25:34 -0700

    Create route for inserting signer signature request info into the signers table.

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,7 +1,18 @@
-const router = require("express").Router();
+const router = require("express").Router()
+const users = require("../../models/db/signers");
 
 router.get("/", (req, res) => {
   res.render("index");
+});
+
+router.post("/nonembedded", (req, res) => {
+  users.create(req.body)//this would be the response info
+  .then(user => {
+    if (user)
+      return res.redirect("/");
+    }
+  )
+  .catch(error => next(error));
 });
 
 module.exports = router;

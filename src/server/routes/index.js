61131a26395975d35256c9842458d159ca895ff6
LaToya Williams <latoyazamill@yahoo.com>
2018-08-01 11:46:37 -0700

    Create a sucessful signature request by parsing the request body and passing the info to hellosign

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router()
 const signers = require("../../models/db/signers");
-const config = require('./config.js');
+const config = require("../../../config.js");
 
 const hellosign = require('hellosign-sdk')({
   key: config.APIKEY
@@ -15,13 +15,35 @@ router.get("/nonembedded", (req, res) => {
 });
 
 router.post("/nonembedded", (req, res) => {
-  signers.nonEmbeddedInfo(req.body)//this would be the response info
-  .then(user => {
-    if (user)
-      return res.redirect("/");
+  console.log("What is this?", req.body);
+  const options = {
+    test_mode: req.body.ufTestMode,
+    title: req.body.ufTitle,
+    subject: req.body.ufSubject,
+    message: req.body.ufMessage,
+    signers: [{
+    email_address: req.body.ufSigner1EmailAddress,
+    name: req.body.ufSigner1Name
+  }],
+    files: [req.body.ufFile1],
+    metadata: {
+      clientId: config.CLIENTID,
+      custom_text: 'NDA #9'
     }
-  )
-  .catch(error => next(error));
+  };
+  hellosign.signatureRequest.send(options)
+      .then(function(response) {
+        console.log("What is the response", response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  // signers.nonEmbeddedInfo(req.body)
+  //   .then(user => {
+  //     if (user)
+  //       return res.redirect("/");
+  //   })
+  //   .catch(error => next(error));
 });
 
 module.exports = router;
